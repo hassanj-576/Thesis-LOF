bf5660dfdde88148bc75e095ab9eb099e1d42b02
Hassan <hassan@gup-128-132.cwi.nl>
2017-03-23 12:44:21 +0100

    New Comit

--- src/main/scala/LOF.scala
+++ src/main/scala/LOF.scala
@@ -15,25 +15,25 @@ object LOF {
 	def main(args: Array[String]) {
 		val conf = new SparkConf().setMaster("local").setAppName("My App")
 		val sc = new SparkContext(conf)
-		val neighbors = getNNeighbors("dataSmall.arff",9,sc)
+		val neighbors = getNNeighbors("dataSmall.csv",10,sc)
 		neighbors.filter(values=>values._1==0).first()._2.foreach(println)
 		//println(neighbors.count())
 		//neighbors.first()._2.foreach(println
 	
 		// neighbors.collect().foreach(println)
-		val kDistance=getKDistance(neighbors,7)
+		val kDistance=getKDistance(neighbors,9)
 		//kDistance.collect().foreach(println)
 		print(kDistance.lookup(0)(0))
 		// val firstRdd= kDistance.filter(values=>values._1._1==0)
 		// print(firstRdd.first())
-		getReachDistance(neighbors,kDistance)
+		val localReachDist = getReachDistance(neighbors,kDistance)
 		// reachDist.filter(values=>values._1==0).first()._2.foreach(println)
 		// val localReachDist=getLocalReachDistance(reachDist)
 		// print (localReachDist.filter(values=>values._1==0).first())
 
-		// val LOF=getLOF(localReachDist,neighbors)
-		// val newLOF=LOF.sortBy(_._2)
-		// newLOF.foreach(println)
+		val LOF=getLOF(localReachDist,neighbors)
+		val newLOF=LOF.sortBy(_._2)
+		newLOF.foreach(println)
 		
 		//Thread.sleep(500000)
 		sc.stop()
@@ -46,13 +46,13 @@ object LOF {
 		val dimension= denseVector.first().size
 		val denseRDDZipped = denseVector.map(values=>(values.toSparse)).zipWithIndex()
 		val finalVector =denseRDDZipped.map(values=>(values._2,values._1)) 
-		val annModel =new ANN(dimensions = dimension, measure = "euclidean").setTables(10).setSignatureLength(64).setBucketWidth(2000).train(finalVector)
+		val annModel =new ANN(dimensions = dimension, measure = "euclidean").setTables(10).setSignatureLength(64).setBucketWidth(20000).train(finalVector)
 		val neighbors = annModel.neighbors(minPoints)
 		neighbors
 	
 	}
 	def getKDistance(neighbors:RDD[(Long, Array[(Long, Double)])],k:Integer):RDD[((Long,Double))]={
-		val rejected = neighbors.filter(values=> values._2.size>k)
+		val rejected = neighbors.filter(values=> values._2.size>=k)
 		println(rejected.count())
 		val newNeighbors=rejected.map(values=>(values._1,values._2.map(x=>x._2).zipWithIndex.map(y=>(y._2,y._1))))
 		println(newNeighbors.first())
@@ -61,9 +61,14 @@ object LOF {
 		kDistance
 	}
 	//def getReachDistance(neighbors:RDD[(Long, Array[(Long, Double)])],kDistance:RDD[((Long,Double))]):RDD[(Long, Array[ Double])]= {
-	def getReachDistance(neighbors:RDD[(Long, Array[(Long, Double)])],kDistance:RDD[((Long,Double))])= {
-		val flatNeighbors = neighbors.flatMap(x=>x)
-		flatNeighbors.collect().foreach(println)
+	def getReachDistance(neighbors:RDD[(Long, Array[(Long, Double)])],kDistance:RDD[((Long,Double))]):RDD[(Long,Double)]= {
+		val flatNeighbors = neighbors.flatMapValues(x=>x).map(values=>(values._2._1,(values._1,values._2._2))).join(kDistance).map(y=>(y._2._1._1,y._2._2.max(y._2._1._2)))
+		flatNeighbors.filter(values=>values._1==0).foreach(println)
+		//flatNeighbors.filter(values=>values._1==4).foreach(println)
+		
+		val localReachDistance = flatNeighbors.combineByKey((values)=> (values.toDouble, 1),(x:(Double,Int), values)=> (x._1 + values, x._2 + 1), (x:(Double,Int), y:(Double,Int))=>(x._1 + y._1, x._2+ y._2)).map(values=>(values._1,(values._2._2/values._2._1)))
+		localReachDistance.filter(values=>values._1==0).foreach(println)
+		localReachDistance
 		// have to calculate this dynamically for each Value based on k
 		//val reachDist=neighbors.map(values=>(values._1,values._2.map(x=>(x._2.max(kDistance.filter(v=>v._1==x._1).first()._2 )))))
 		//val reachDist=neighbors.map(values=>(values._1._1,values._2.map(x=>(x._2.max(kDistance.lookup(x._1)(0))))))
@@ -77,8 +82,12 @@ object LOF {
 		val neighborWithReach=neighbors.join(reachDist).map(values=>((values._1,values._2._2),values._2._1))
 		val flat = neighborWithReach.flatMapValues(x=>x).map(values=>(values._2._1,values._1))
 		val result=flat.join(reachDist).map(values=>(values._2._1._1,(values._2._1._2,values._2._2)))
-		val LOF = result.combineByKey((v) => (v._2.toDouble / v._1, 1),(acc: (Double, Int), q:((Double,Double))) => ((q._2/q._1)+acc._1,acc._2+1),(acc1: (Double, Int), acc2: (Double, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)).map(values=>(values._1,(values._2._1/values._2._2)))
-		LOF
+		result.filter(values=>values._1==0).foreach(println)
+		//val LOF = result.combineByKey((v) => (v._2.toDouble / v._1, 1),(acc: (Double, Int), q:((Double,Double))) => ((q._2/q._1)+acc._1,acc._2+1),(acc1: (Double, Int), acc2: (Double, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)).map(values=>(values._1,(values._2._1/values._2._2)))
+		
+		val LOF = result.combineByKey((v) => (v._2.toDouble / v._1, 1),(acc: (Double, Int), q:((Double,Double))) => ((q._2/q._1)+acc._1,acc._2+1),(acc1: (Double, Int), acc2: (Double, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+		LOF.filter(values=>values._1==0).foreach(println)
+		LOF.map(values=>(values._1,(values._2._1/values._2._2)))
 	}
 	
 	
