76c8b0963f81ed3d58d916a56f629bda396e6aaf
Hassan <hassan@gup-128-132.cwi.nl>
2017-03-24 17:51:56 +0100

    Main Class created

---
+++ src/main/scala/maiClass.scala
@@ -0,0 +1,29 @@
+package main.scala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.linalg.Vectors
+import collection.JavaConversions._
+import org.apache.spark.SparkContext._
+import java.io.StringReader
+import com.opencsv.CSVReader;
+import com.github.karlhigley.spark.neighbors.ANN
+import org.apache.spark.rdd.RDD
+
+
+object mainClass {
+	def main(args: Array[String]) {
+		val conf = new SparkConf().setMaster("local").setAppName("My App")
+		val sc = new SparkContext(conf)
+		val lofWrapper = new LOFWrapper("dataSmall.csv",10,sc,550)
+		val lofVal = lofWrapper.getLOF()
+		lofVal.foreach(println)
+		// val fileName=args(0)
+		// val k = args(1).toInt
+		// val bucketWidth = args(2).toInt
+		
+		sc.stop()
+		
+	}
+	
+}
