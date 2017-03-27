b790935b225e78fbd9ae7837442fb338db9899ad
Hassan <hassan@gup-128-132.cwi.nl>
2017-03-27 11:28:37 +0200

    Added K list For test only

--- src/main/scala/mainClass.scala
+++ src/main/scala/mainClass.scala
@@ -9,22 +9,29 @@ import java.io.StringReader
 import com.opencsv.CSVReader;
 import com.github.karlhigley.spark.neighbors.ANN
 import org.apache.spark.rdd.RDD
+import scala.collection.mutable.ArrayBuffer
 
 
 object mainClass {
 	def main(args: Array[String]) {
 		val fileName=args(0)
-		val k = args(1).toInt
-		val bucketWidth = args(2).toInt
-		val conf = new SparkConf().setMaster("local").setAppName("My App")
-		val sc = new SparkContext(conf)
-		val lofWrapper = new LOFWrapper(fileName,k,sc,bucketWidth)
-		val lofVal = lofWrapper.getLOF()
-		lofVal.sortBy(_._2).foreach(println)
+		//val k = args(1).toInt
+		val bucketWidth = args(1).toInt
+		val kList= ArrayBuffer[Int]()
+		val k=10;
+		for ( a <- 2 to args.length){
+			kList+=args(a).toInt
+		}
+		println( kList)
+		// val conf = new SparkConf().setMaster("local").setAppName("My App")
+		// val sc = new SparkContext(conf)
+		// val lofWrapper = new LOFWrapper(fileName,k,sc,bucketWidth)
+		// val lofVal = lofWrapper.getLOF()
+		// lofVal.sortBy(_._2).foreach(println)
 		
 
 		//Comment from stones		
-		sc.stop()
+		//sc.stop()
 		
 	}
 	
