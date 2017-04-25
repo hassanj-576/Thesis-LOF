66cf85f5937ed37e39e602f67406139b9b4d41c4
Hassan <hassan@gup-128-132.cwi.nl>
2017-04-25 15:34:31 +0200

    Auto Script Commit

--- src/main/scala/LOFClass.scala
+++ src/main/scala/LOFClass.scala
@@ -38,7 +38,7 @@ class LOFClass () {
 	}
 	def getKDistance(neighborsDF:DataFrame,k:Integer):DataFrame={
 		val rejected=neighborsDF.where("size(_2)=="+k)
-		rejected.select("_2[k]").show()
+		rejected.select("_2[10]").show()
 		val kDistance=
 		
 		rejected.show()
