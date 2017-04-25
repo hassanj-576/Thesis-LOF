9d8b9644501bcd587577ae2ba7c6e63ac3e5dbcf
Hassan <hassan@gup-128-132.cwi.nl>
2017-04-25 14:37:48 +0200

    trying

--- src/main/scala/LOFClass.scala
+++ src/main/scala/LOFClass.scala
@@ -34,9 +34,9 @@ class LOFClass () {
 		neighborsDF
 	
 	}
-	def getKDistance(neighborsDF:DataFrame,k:Integer):RDD[((Long,Double))]={
-		neighborsDF.where("size(_2)=="+k).show()
-		//rejected
+	def getKDistance(neighborsDF:DataFrame,k:Integer):DataFrame={
+		rejected=neighborsDF.where("size(_2)=="+k)
+		rejected.show()
 		// val newNeighbors=rejected.map(values=>(values._1,values._2.map(x=>x._2).zipWithIndex.map(y=>(y._2,y._1))))
 		// val kDistance = newNeighbors.map(values=> ((values._1,values._2.filter(x=>x._1==k)(0)._2)))
 		// kDistance
