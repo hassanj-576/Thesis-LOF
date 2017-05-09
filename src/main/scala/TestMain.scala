eea29dea55626927a5f5ac5d43b451bf27c94d80
Hassan <hassan@Hassans-MacBook-Pro.local>
2017-05-09 21:52:23 +0200

    moved some stuff

--- src/main/scala/TestMain.scala
+++ src/main/scala/TestMain.scala
@@ -24,10 +24,10 @@ object TestMain {
 		}
 		val textFile = sc.textFile(fileName)
 		testRdd= textFile.map(x=>x.toInt+1)
-		testRdd.setName("testRdd")
 		
-		println("RDD COUNT: "+testRdd.count)
 		testRdd.cache
+		println("RDD COUNT: "+testRdd.count)
+		
 		
 		val newTestRdd = testRdd.map(x=>x.toInt+1)
 		
@@ -41,6 +41,7 @@ object TestMain {
 		// testRdd.foreach(println)
 		println("Printing new test rdd")
 		newTestRdd.foreach(println)
+		testRdd.setName("testRdd")
 	}
 
   }
