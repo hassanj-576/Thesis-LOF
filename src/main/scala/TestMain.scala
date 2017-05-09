2e0dc416997106aa61ad124ca5c4658a7691db85
Hassan <hassan@gup-128-132.cwi.nl>
2017-05-09 19:23:29 +0200

    made a few changes

--- src/main/scala/TestMain.scala
+++ src/main/scala/TestMain.scala
@@ -17,19 +17,23 @@ object TestMain {
   	println(textFile.count())
   	type rddType = (Long)
 	var testRdd = sc.emptyRDD[rddType]
-
+	var fileName="rddInput.txt"
 	for( a <- 1 to 2){
-	testRdd= textFile.map(x=>x.toInt+1)
-	val newTestRdd = testRdd.map(x=>x.toInt+1)
-	testRdd.setName("testRdd")
-	//println(testRdd.count)
-	println("Iteration: "+a)
-	//testRdd.foreach(x=> println("Value: "+x))
-	//println("Test RDD Size: "+testRdd.count)
-	println("Printing the entire thing")
-	testRdd.foreach(println)
-	println("New Test RDD: "+newTestRdd.count)
-	testRdd.cache()
+		if(a==2){
+			fileName="rddInput2.txt"
+		}
+		val textFile = sc.textFile(fileName)
+		testRdd= textFile.map(x=>x.toInt+1)
+		val newTestRdd = testRdd.map(x=>x.toInt+1)
+		testRdd.setName("testRdd")
+		//println(testRdd.count)
+		println("Iteration: "+a)
+		//testRdd.foreach(x=> println("Value: "+x))
+		//println("Test RDD Size: "+testRdd.count)
+		println("Printing the entire thing")
+		testRdd.foreach(println)
+		println("New Test RDD: "+newTestRdd.count)
+		testRdd.cache()
 	}
 
   }
