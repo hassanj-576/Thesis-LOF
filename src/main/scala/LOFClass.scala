fe1d010954110b40eb7d8740a6dabe23719b192b
Hassan <hassan@Hassans-MacBook-Pro.local>
2017-05-09 23:08:00 +0200

    complex if

--- src/main/scala/LOFClass.scala
+++ src/main/scala/LOFClass.scala
@@ -40,6 +40,12 @@ class LOFClass () {
 		println("Rejected Count :"+rejected.count)
 		val newNeighbors=rejected.map(values=>(values._1,values._2.map(x=>x._2).zipWithIndex.map(y=>(y._2,y._1))))
 		newNeighbors.first()._2.foreach(println)
+		newNeighbors.foreach{
+			x=>
+			if(x._2.size<k){
+				println(x._2.size)
+			}
+		}
 		val kDistance = newNeighbors.map(values=> ((values._1,values._2.filter(x=>x._1==k)(0)._2)))
         kDistance
 	}
