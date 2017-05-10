1e6115415352915db88e43f74f513994047eccc0
Hassan <hassan@gup-128-132.cwi.nl>
2017-05-10 15:14:04 +0200

    fixed problem with Build.sbt

--- src/main/scala/TestMain.scala
+++ src/main/scala/TestMain.scala
@@ -1,60 +1,60 @@
-package main.scala
+// package main.scala
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import collection.JavaConversions._
-import org.apache.spark.rdd.RDD
+// import org.apache.spark.SparkConf
+// import org.apache.spark.SparkContext
+// import org.apache.spark.SparkContext._
+// import collection.JavaConversions._
+// import org.apache.spark.rdd.RDD
 
 
-object TestMain {
+// object TestMain {
   
-  def main(args: Array[String]) {
-  	val conf = new SparkConf().setMaster("local").setAppName("My App")
-	val sc = new SparkContext(conf)
-  	println("Hello World")
-  	val textFile = sc.textFile("rddInput.txt")
-  	println(textFile.count())
-  	type rddType = (Long)
-	var testRdd = sc.emptyRDD[rddType]
-	var fileName="rddInput.txt"
-	for( a <- 1 to 5){
-		println()
-		println()
-		println("Iteration: "+a)
-		println()
-		println()
-		if(a==2){
-			fileName="rddInput2.txt"
+//   def main(args: Array[String]) {
+//   	val conf = new SparkConf().setMaster("local").setAppName("My App")
+// 	val sc = new SparkContext(conf)
+//   	println("Hello World")
+//   	val textFile = sc.textFile("rddInput.txt")
+//   	println(textFile.count())
+//   	type rddType = (Long)
+// 	var testRdd = sc.emptyRDD[rddType]
+// 	var fileName="rddInput.txt"
+// 	for( a <- 1 to 5){
+// 		println()
+// 		println()
+// 		println("Iteration: "+a)
+// 		println()
+// 		println()
+// 		if(a==2){
+// 			fileName="rddInput2.txt"
 
-		}
-		val textFile = sc.textFile(fileName)
-		testRdd= textFile.map(x=>x.toInt+1)
-		testRdd.setName("testRdd")
-		//testRdd.setName("somethingelse")
-		println("PRINTING TEST RDD : "+testRdd.count)
-		//testRdd.cache
-		testRdd.cache
-		val newTestRdd = testRdd.map(x=>x.toInt+1)
+// 		}
+// 		val textFile = sc.textFile(fileName)
+// 		testRdd= textFile.map(x=>x.toInt+1)
+// 		testRdd.setName("testRdd")
+// 		//testRdd.setName("somethingelse")
+// 		println("PRINTING TEST RDD : "+testRdd.count)
+// 		//testRdd.cache
+// 		testRdd.cache
+// 		val newTestRdd = testRdd.map(x=>x.toInt+1)
 		
-		//println(testRdd.count)
+// 		//println(testRdd.count)
 		
-		//testRdd.foreach(x=> println("Value: "+x))
-		//println("Test RDD Size: "+testRdd.count)
-		// println("Printing the entire thing")
-		// testRdd.foreach(println)
-		// println("Printing the entire thing Second Time")
-		// testRdd.foreach(println)
-		println("Printing new test rdd")
-		newTestRdd.foreach(println)
-		//testRdd.setName("testRdd")
-	}
-	for ((id: Int,rdd: org.apache.spark.rdd.RDD[_])<- sc.getPersistentRDDs ){
-		  println("PERSISTANT RDD NAME: "+rdd.name)
-	}
+// 		//testRdd.foreach(x=> println("Value: "+x))
+// 		//println("Test RDD Size: "+testRdd.count)
+// 		// println("Printing the entire thing")
+// 		// testRdd.foreach(println)
+// 		// println("Printing the entire thing Second Time")
+// 		// testRdd.foreach(println)
+// 		println("Printing new test rdd")
+// 		newTestRdd.foreach(println)
+// 		//testRdd.setName("testRdd")
+// 	}
+// 	for ((id: Int,rdd: org.apache.spark.rdd.RDD[_])<- sc.getPersistentRDDs ){
+// 		  println("PERSISTANT RDD NAME: "+rdd.name)
+// 	}
 
-  }
+//   }
 
-}
+// }
 
-//   
+// //   
