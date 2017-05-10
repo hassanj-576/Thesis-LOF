bb047ff40d3d0fe67c9dc88a6fab6d5a8d8c6fc2
Hassan <hassan@gup-128-132.cwi.nl>
2017-05-10 18:03:24 +0200

    Why :(

--- src/main/scala/LOFWrapper.scala
+++ src/main/scala/LOFWrapper.scala
@@ -25,6 +25,11 @@ class LOFWrapper(faster:Int,fName:String,kPoints:ArrayBuffer[Int],sContext:Spark
 		val LOFList= ArrayBuffer[RDD[(Long,Double)]]()
 		val LOFvar = new LOFClass()
 		for (x <- sortedList) {
+			println()
+			println()
+			println("Iteration: "+x)
+			println()
+			println()
 			val nRdd = sc.parallelize(Array(x))
 			nRdd.setName("nRdd")
 			nRdd.cache
