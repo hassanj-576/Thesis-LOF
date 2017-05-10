964d82b1bb9b4421e499b178d07568ab383599a3
Hassan <hassan@gup-128-132.cwi.nl>
2017-05-10 16:41:03 +0200

    Moment of truth

--- src/main/scala/TestMain3.scala
+++ src/main/scala/TestMain3.scala
@@ -1,49 +1,49 @@
-package main.scala
+// package main.scala
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import collection.JavaConversions._
-import org.apache.spark.rdd.RDD
+// import org.apache.spark.SparkConf
+// import org.apache.spark.SparkContext
+// import org.apache.spark.SparkContext._
+// import collection.JavaConversions._
+// import org.apache.spark.rdd.RDD
 
 
-object TestMain3 {
+// object TestMain3 {
   
-  def main(args: Array[String]) {
-  	val conf = new SparkConf().setMaster("local").setAppName("My App")
-	val sc = new SparkContext(conf)
-  	println("Hello World")
-  	val textFile = sc.textFile("rddInput.txt")
-  	println(textFile.count())
-  	type rddType = (Long)
-	var testRdd = sc.emptyRDD[rddType]
-	var fileName="rddInput.txt"
-	for( a <- 1 to 5){
-		val nRdd = sc.parallelize(Array(a))
-		nRdd.setName("nRdd")
-		nRdd.cache
-		nRdd.count
+//   def main(args: Array[String]) {
+//   	val conf = new SparkConf().setMaster("local").setAppName("My App")
+// 	val sc = new SparkContext(conf)
+//   	println("Hello World")
+//   	val textFile = sc.textFile("rddInput.txt")
+//   	println(textFile.count())
+//   	type rddType = (Long)
+// 	var testRdd = sc.emptyRDD[rddType]
+// 	var fileName="rddInput.txt"
+// 	for( a <- 1 to 5){
+// 		val nRdd = sc.parallelize(Array(a))
+// 		nRdd.setName("nRdd")
+// 		nRdd.cache
+// 		nRdd.count
 
-		var nInt=0
-		for ((id: Int,rdd: org.apache.spark.rdd.RDD[_])<- sc.getPersistentRDDs ){
-			println("PERSISTANT RDD NAME: "+rdd.name)
-		  	if(rdd.name=="nRdd"){
-		  		nInt=rdd.asInstanceOf[RDD[Int]].first()
-		  		println("nInt var: "+nInt)
-		  		println("nInt class: "+nInt.getClass)
-		  		rdd.unpersist()
-		  }
-		}
+// 		var nInt=0
+// 		for ((id: Int,rdd: org.apache.spark.rdd.RDD[_])<- sc.getPersistentRDDs ){
+// 			println("PERSISTANT RDD NAME: "+rdd.name)
+// 		  	if(rdd.name=="nRdd"){
+// 		  		nInt=rdd.asInstanceOf[RDD[Int]].first()
+// 		  		println("nInt var: "+nInt)
+// 		  		println("nInt class: "+nInt.getClass)
+// 		  		rdd.unpersist()
+// 		  }
+// 		}
 
-	}
-		for ((id: Int,rdd: org.apache.spark.rdd.RDD[_])<- sc.getPersistentRDDs ){
-			println("PERSISTANT RDD NAME: "+rdd.name)
+// 	}
+// 		for ((id: Int,rdd: org.apache.spark.rdd.RDD[_])<- sc.getPersistentRDDs ){
+// 			println("PERSISTANT RDD NAME: "+rdd.name)
 		 
-		}
+// 		}
 	
 
-  }
+//   }
 
-}
+// }
 
-//   
+// //   
