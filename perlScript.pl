21930a1d413798e5596572e44bff1b50714c5490
Hassan <hassan@Hassans-MacBook-Pro.local>
2017-06-12 10:59:14 +0200

    testing perl script

--- perlScript.pl
+++ perlScript.pl
@@ -4,7 +4,7 @@ use Carp;
 use English qw(-no_match_vars);
 
 
-my $files = 'output.csv';
+my $files = 'output2.csv';
 if (-f $files) {
 	unlink $files
 		or croak "Cannot delete $files: $!";
@@ -25,7 +25,7 @@ print { $OUTFILE } "N,iteration,fast,Time,\n";
 my $txt=".csv";
 my $output="output";
 my $N;
-for($N=100;$N<=10000;$N=$N+100){
+for($N=100;$N<=500;$N=$N+100){
 	my $outputFile = `python dataGenerator.py $N$txt 1 $N 5`;
 	for($a=0;$a<1;$a=$a+1){
 			print { $OUTFILE } "$N,$a,0,";
