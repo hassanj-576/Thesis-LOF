cc294bad9b60fefc5a7818a81fb86622c4060884
Hassan <hassan@gup-128-132.cwi.nl>
2017-06-16 13:34:41 +0200

    I am an idiot

--- src/main/scala/LOFClass.scala
+++ src/main/scala/LOFClass.scala
@@ -27,10 +27,11 @@ class LOFClass () {
 		val dimension= denseVector.first().size
 		val denseRDDZipped = denseVector.map(values=>(values.toSparse)).zipWithIndex()
 		val finalVector =denseRDDZipped.map(values=>(values._2,values._1)) 
-		val annModel =new ANN(dimensions = dimension, measure = "euclidean").setTables(10).setSignatureLength(64).setBucketWidth(bucketWidth).train(finalVector)
+		val annModel =new ANN(dimensions = dimension, measure = "euclidean").setTables(1).setSignatureLength(64).setBucketWidth(bucketWidth).train(finalVector)
 		val neighbors = annModel.neighbors(minPoints)
+		
 		val errorNeighbors = neighbors.filter(values=> values._2.size<minPoints)
-		println("Error Values: "+minPoints)
+		println("Error Values: "+errorNeighbors)
 		neighbors
 	
 	}
